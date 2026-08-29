8feab2710ff40be3a22f48d6203783d73de65441
MD NAHID HASAN SHUVO <43151075+nahid15@users.noreply.github.com>
2026-08-29 17:09:00 -0400

    Add: majority-element - submission-11

---
+++ Data Structures & Algorithms/majority-element/submission-11.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        ans = 0
+        #voting algorithm O(1) space
+        for i in nums:
+            if count == 0:
+                count += 1
+                ans = i
+            if i == nums[0]:
+                count += 1
+            else:
+                count -= 1
+        return ans
